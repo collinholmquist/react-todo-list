51fad954d060f2fce459a91f3215f3bddd952316
Collin Holmquist <collinholmquist@gmail.com>
2020-04-12 15:27:48 -0500

    added props & getting data from fake api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import TodoItem from './TodoItem'
 import './App.css'
+import todosData from './todosData'
 
 function App() {
+  
+  const todoComponents = todosData.map(item => <TodoItem key = {item.id} item={item} />) 
+
   return (
     <div className = "todo-list">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem /> 
+        {todoComponents} 
     </div>
-  );
+  )
 }
 
 export default App;
